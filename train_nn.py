6022e736e14047ea15aa1370147971f7fad19a88
Your Name <you@example.com>
2017-06-21 02:23:49 +0000

    feat: Changing Network Architecture and Adding generators to make code more efficient.

--- train_nn.py
+++ train_nn.py
@@ -1,125 +1,127 @@
-#!/usr/bin/env python3
-from subprocess import check_output
+#!/usr/bin/env python
+
 from glob import glob
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
-from sklearn.preprocessing import LabelBinarizer
+import sklearn
 from random import shuffle
 #import seaborn as sns
+import cv2
+import matplotlib.pyplot as plt
+from keras.layers import Flatten,Dense,Convolution2D,MaxPooling2D,Dropout,Activation,Lambda,Cropping2D
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 # TODO: Build the Final Test Neural Network in Keras Here
-def lenet(input_shape):
-    print("input_shape",input_shape)
-    model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=input_shape))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.5))
+def lenet(input_shape=(640,480,3)):
+    """LeNet architecture(final solution)"""
+    model=Sequential()
+    model.add(Lambda(lambda x: (x-128.0)/128.0,input_shape=input_shape))
+    #model.add(Cropping2D(cropping=((70,25), (0,0))))
+
+    # SOLUTION: Layer 1: Convolutional. Input =  Output = 
+    model.add(Convolution2D(nb_filter=6, nb_row=5,nb_col=5, subsample=(1,1),bias=True))
+
+    # SOLUTION: Activation.
     model.add(Activation('relu'))
+
+
+    # SOLUTION: Pooling. Input =  Output = 
+    model.add(MaxPooling2D(pool_size=(2, 2),border_mode="valid"))
+
+    # SOLUTION: Layer 2: Convolutional. Output = 
+    model.add(Convolution2D(nb_filter=16, nb_row=5,nb_col=5, subsample=(1,1),bias=True))
+    
+    # SOLUTION: Activation.
+    model.add(Activation('relu'))
+
+    # SOLUTION: Pooling. Input =  Output = 
+    model.add(MaxPooling2D(pool_size=(2, 2),border_mode="valid"))
+
+    # SOLUTION: Flatten. Input = 5x5x16. Output = .
     model.add(Flatten())
-    model.add(Dense(128))
+    
+    # SOLUTION: Layer 3: Fully Connected. Input = . Output = .
+    model.add(Dense(120,bias=True))
+    
+    # SOLUTION: Activation.
     model.add(Activation('relu'))
-    model.add(Dense(3))
-    model.add(Activation('softmax'))
-    model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+
+    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
+    model.add(Dense(84,bias=True))
+    
+    # SOLUTION: Activation.
+    model.add(Activation('relu'))
+
+    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 3.
+    model.add(Dense(3,activation="softmax"))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-import sklearn
-from sklearn.preprocessing import LabelEncoder,Normalizer,StandardScaler
-from sklearn import svm
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV, train_test_split
+from common_functions import get_label,timeit
+label_binarizer = LabelBinarizer()
+label_binarizer.fit([0,1,2])
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
 
-from common_functions import *
-color_space = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 18  # HOG orientations #18
-pix_per_cell = 40# HOG pixels per cell. Was 16
-cell_per_block = 2 # HOG cells per block
-hog_channel = "ALL" # Can be 0, 1, 2, or "ALL". Was ALL
-spatial_size = (32, 32) # Spatial binning dimensions
-hist_bins = 32    # Number of histogram bins
-spatial_feat = False # Spatial features on or off
-hist_feat = False # Histogram features on or off
-hog_feat = False # HOG features on or off
-min_size =(640,480)
-vertical_crop=0.15#15% crop on either side
-#y_start_stop = [400, 720] # Min and max in y to search in slide_window()
-experiment_num="1k"
-model_file_name="clf_%s.pkl"%experiment_num
-data_file_name="data_%s.npz"%experiment_num
-scaler_file_name="standard_scaler_%s.pkl"%experiment_num
-predictions_file_name="predictions_%s.csv"%experiment_num
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            labels = []
+            for filepath in batch_samples:
+                #Data Augmentation 2: Flipping Image Laterally
+                img=cv2.imread(filepath)
+                if img is None:
+                    continue
 
-@timeit
-def load_train_data(train_files):
-    features,labels = extract_features(train_files,
-                                       color_space=color_space, 
-                                       spatial_size=spatial_size, 
-                                       hist_bins=hist_bins, 
-                                       orient=orient, 
-                                       pix_per_cell=pix_per_cell, 
-                                       cell_per_block=cell_per_block, 
-                                       hog_channel=hog_channel, 
-                                spatial_feat=spatial_feat, 
-                                       hist_feat=hist_feat, 
-                                       hog_feat=hog_feat,
-                                       new_size=min_size,
-                                       crop=vertical_crop)
-    X = np.array(features).astype(np.float64)
-    print ("X.shape",X.shape)
-    scaled_X = np.array(X / 255.0 - 0.5 )
-    # Fit a per-column scaler
-    #X_scaler = StandardScaler().fit(X)
-    # Apply the scaler to X
-    #scaled_X = X_scaler.transform(X)
+                img=img[...,::-1]#flip BGR to RGB
+                rows=640
+                cols=480
+                cv2_shape=(cols,rows)
+                color1 = cv2.resize(img[:,:,0],cv2_shape )
+                color2 = cv2.resize(img[:,:,1], cv2_shape)
+                color3 = cv2.resize(img[:,:,2], cv2_shape)
+                img=np.dstack((color1, color2, color3))
+                
+                #flipped_image = np.fliplr(original_image)
+                images.append(img)
+                labels.append(get_label(filepath))
 
-    # Define the labels vector
-    y = np.array(labels)
-    print ("labels",labels)
-    return scaled_X,y
-    #return scaled_X,y,X_scaler
+# trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(labels)
+            
+            y_train = label_binarizer.transform(y_train)
+            print("X_train.shape",X_train.shape)
+            print("y_train.shape",y_train.shape)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+def train_model(model,train_samples,validation_samples,batch_size):
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
+
+    history=model.fit_generator(train_generator, 
+                                samples_per_epoch= len(train_samples)*6, 
+                                validation_data=validation_generator,
+                                nb_val_samples=len(validation_samples)*6, nb_epoch=3)
+    return history
+
 
 @timeit
-def train_model(X,y,random_state=42):
-    input_shape=X[0].shape
-    model=lenet(input_shape)
-    print("Training data shape",X.shape,"Training label shape",y.shape)
-    history = model.fit(X, y, nb_epoch=10, validation_split=0.2)
-    return model
+def train_model(model,train_samples,validation_samples,batch_size):
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
 
-@timeit
-def load_test_data(test_files,X_scaler=None):
-    test_image_df=pd.DataFrame(pd.DataFrame({'imagepath': test_files}))
-    test_features,_ = extract_features(test_files, 
-                                       color_space=color_space, 
-                                       spatial_size=spatial_size, 
-                                       hist_bins=hist_bins, 
-                                       orient=orient, 
-                                       pix_per_cell=pix_per_cell, 
-                                       cell_per_block=cell_per_block, 
-                                       hog_channel=hog_channel, 
-                                       spatial_feat=spatial_feat, 
-                                       hist_feat=hist_feat, 
-                                       hog_feat=hog_feat,
-                                       new_size=min_size,
-                                       crop=vertical_crop)
+    history=model.fit_generator(train_generator, 
+                                samples_per_epoch= len(train_samples)*6, 
+                                validation_data=validation_generator,
+                                nb_val_samples=len(validation_samples)*6, nb_epoch=3)
+    return history
 
-    X = np.array(test_features).astype(np.float64)
-
-    #X = np.vstack((test_features)).astype(np.float64)  
-    # Fit a per-column scaler
-    # Apply the scaler to X
-    scaled_X = np.array(X / 255.0 - 0.5 )
-    return test_image_df,scaled_X
 
 @timeit
 def main(base,samples=5):
@@ -127,38 +129,40 @@ def main(base,samples=5):
     #test_files+= glob("/home/u3920/cervix_type_prediction/test_data/test_stg2/*.jpg")
 
     train_files = glob(base+'train/*/*.jpg')
+
     # Additional files
     #train_files+= glob("/data/kaggle_3.27/additional/*/*.jpg")
     shuffle(train_files)
-
     train_files=train_files[0:samples]
+    train_samples, validation_samples = train_test_split(train_files, test_size=0.2)
+    old_model_name=None
+    model_name="model"
+    model=lenet()
+    if old_model_name:
+        model.load_weights(old_model_name+"_weights.h5")
 
-    X,y=load_train_data(train_files)
-    label_binarizer = LabelBinarizer()
-    y_one_hot = label_binarizer.fit_transform(y)
-    cv=train_model(X,y_one_hot)
-    
-    # try:
-    #     joblib.dump(cv,model_file_name)
-    # except:
-    #     pass
+    print("train_samples",len(train_samples),train_samples[0])
+    print("validation_samples",len(validation_samples),validation_samples[0])
+    history=train_model(model,train_samples,validation_samples,batch_size=64)
+    model.save(model_name+".h5")
+    model.save_weights(model_name+"_weights.h5")
 
-    predictions_df=pd.DataFrame()
-    for x in range(0,len(test_files),400):
-        start=x
-        end=min(x+400,len(test_files))
-        test_image_df,test_imgs_mat = load_test_data(test_files[start:end],X_scaler)
-        preds=cv.predict(test_imgs_mat)
-        print("Test set predictions shape",preds.shape)
-        test_image_df["Type_1"]=preds[:,0]
-        test_image_df["Type_2"]=preds[:,1]
-        test_image_df["Type_3"]=preds[:,2]
-        predictions_df=predictions_df.append(test_image_df,ignore_index=True)
+    # predictions_df=pd.DataFrame()
+    # for x in range(0,len(test_files),400):
+    #     start=x
+    #     end=min(x+400,len(test_files))
+    #     test_image_df,test_imgs_mat = load_test_data(test_files[start:end],X_scaler)
+    #     preds=cv.predict(test_imgs_mat)
+    #     print("Test set predictions shape",preds.shape)
+    #     test_image_df["Type_1"]=preds[:,0]
+    #     test_image_df["Type_2"]=preds[:,1]
+    #     test_image_df["Type_3"]=preds[:,2]
+    #     predictions_df=predictions_df.append(test_image_df,ignore_index=True)
 
-    func=lambda x: x.split("/")[-1]
-    predictions_df["image_name"]=predictions_df["imagepath"].apply(func)
-    predictions_df[["image_name","Type_1","Type_2","Type_3"]].to_csv(predictions_file_name,index=False)
-    
+    # func=lambda x: x.split("/")[-1]
+    # predictions_df["image_name"]=predictions_df["imagepath"].apply(func)
+    # predictions_df[["image_name","Type_1","Type_2","Type_3"]].to_csv(predictions_fi
+
 if __name__=="__main__":
-    basepath="/Users/g/mywork/cervix_type_prediction/"
-    main(basepath,20)
+    basepath="/home/carnd/cervix_type_prediction/data/"
+    main(basepath,30)
