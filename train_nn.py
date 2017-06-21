c5cc546eb916ec426c42f9596998cabcecb63df6
you@example.com <you@example.com>
2017-06-21 16:05:34 +0000

    feat: Adding dropout and separate functions for test set i. Convnet seems to be overfitting ii. Setup separate generator for test set since the test set  doesnt have labels

--- train_nn.py
+++ train_nn.py
@@ -12,8 +12,9 @@ from keras.layers import Flatten,Dense,Convolution2D,MaxPooling2D,Dropout,Activa
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
-# TODO: Build the Final Test Neural Network in Keras Here
-def lenet(input_shape=(640,480,3)):
+
+input_shape=(128,96,3)#640,480,3
+def lenet(input_shape=input_shape):
     """LeNet architecture(final solution)"""
     model=Sequential()
     model.add(Lambda(lambda x: (x-128.0)/128.0,input_shape=input_shape))
@@ -25,10 +26,11 @@ def lenet(input_shape=(640,480,3)):
     # SOLUTION: Activation.
     model.add(Activation('relu'))
 
-
     # SOLUTION: Pooling. Input =  Output = 
     model.add(MaxPooling2D(pool_size=(2, 2),border_mode="valid"))
 
+    model.add(Dropout(0.25))
+    
     # SOLUTION: Layer 2: Convolutional. Output = 
     model.add(Convolution2D(nb_filter=16, nb_row=5,nb_col=5, subsample=(1,1),bias=True))
     
@@ -37,6 +39,8 @@ def lenet(input_shape=(640,480,3)):
 
     # SOLUTION: Pooling. Input =  Output = 
     model.add(MaxPooling2D(pool_size=(2, 2),border_mode="valid"))
+
+    model.add(Dropout(0.25))
 
     # SOLUTION: Flatten. Input = 5x5x16. Output = .
     model.add(Flatten())
@@ -61,6 +65,27 @@ def lenet(input_shape=(640,480,3)):
 from common_functions import get_label,timeit
 label_binarizer = LabelBinarizer()
 label_binarizer.fit([0,1,2])
+
+def load_image(filepath):
+    img=cv2.imread(filepath)
+    if img is None:
+        return None
+    
+    img=img[...,::-1]#flip BGR to RGB
+    # Crop the image
+    top=int(img.shape[0]*0.15)
+    bottom=int(img.shape[0]*0.85)
+    img=img[top:bottom,:,:]
+    
+    rows=input_shape[0]
+    cols=input_shape[1]
+    cv2_shape=(cols,rows)
+    color1 = cv2.resize(img[:,:,0],cv2_shape )
+    color2 = cv2.resize(img[:,:,1], cv2_shape)
+    color3 = cv2.resize(img[:,:,2], cv2_shape)
+    img=np.dstack((color1, color2, color3))
+    return img
+    
 def generator(samples, batch_size=32):
     num_samples = len(samples)
 
@@ -72,20 +97,10 @@ def generator(samples, batch_size=32):
             labels = []
             for filepath in batch_samples:
                 #Data Augmentation 2: Flipping Image Laterally
-                img=cv2.imread(filepath)
+                #flipped_image = np.fliplr(original_image)
+                img=load_image(filepath)
                 if img is None:
                     continue
-
-                img=img[...,::-1]#flip BGR to RGB
-                rows=640
-                cols=480
-                cv2_shape=(cols,rows)
-                color1 = cv2.resize(img[:,:,0],cv2_shape )
-                color2 = cv2.resize(img[:,:,1], cv2_shape)
-                color3 = cv2.resize(img[:,:,2], cv2_shape)
-                img=np.dstack((color1, color2, color3))
-                
-                #flipped_image = np.fliplr(original_image)
                 images.append(img)
                 labels.append(get_label(filepath))
 
@@ -94,10 +109,29 @@ def generator(samples, batch_size=32):
             y_train = np.array(labels)
             
             y_train = label_binarizer.transform(y_train)
-            print("X_train.shape",X_train.shape)
-            print("y_train.shape",y_train.shape)
             yield sklearn.utils.shuffle(X_train, y_train)
 
+
+def test_generator(samples, batch_size=32):
+    num_samples = len(samples)
+
+    for offset in range(0, num_samples, batch_size):
+        start=offset
+        end=min(offset+batch_size,num_samples)
+        print("start","end",start,end)
+        batch_samples = samples[start:end]
+        images = []
+        for filepath in batch_samples:
+                #Data Augmentation 2: Flipping Image Laterally
+                #flipped_image = np.fliplr(original_image)
+            img=load_image(filepath)
+            if img is None:
+                continue
+            images.append(img)
+
+        X_test = np.array(images)
+        yield X_test
+
 def train_model(model,train_samples,validation_samples,batch_size):
     # compile and train the model using the generator function
     train_generator = generator(train_samples, batch_size=batch_size)
@@ -106,34 +140,23 @@ def train_model(model,train_samples,validation_samples,batch_size):
     history=model.fit_generator(train_generator, 
                                 samples_per_epoch= len(train_samples)*6, 
                                 validation_data=validation_generator,
-                                nb_val_samples=len(validation_samples)*6, nb_epoch=3)
+                                nb_val_samples=len(validation_samples)*6, nb_epoch=10)
     return history
-
-
-@timeit
-def train_model(model,train_samples,validation_samples,batch_size):
-    # compile and train the model using the generator function
-    train_generator = generator(train_samples, batch_size=batch_size)
-    validation_generator = generator(validation_samples, batch_size=batch_size)
-
-    history=model.fit_generator(train_generator, 
-                                samples_per_epoch= len(train_samples)*6, 
-                                validation_data=validation_generator,
-                                nb_val_samples=len(validation_samples)*6, nb_epoch=3)
-    return history
-
 
 @timeit
 def main(base,samples=5):
-    test_files = glob(base+'test/*.jpg')
-    #test_files+= glob("/home/u3920/cervix_type_prediction/test_data/test_stg2/*.jpg")
+    test_files = glob(base+"test/*.jpg")
+    test_files+= glob(base+"test_stg2/*.jpg")
 
-    train_files = glob(base+'train/*/*.jpg')
-
+    train_files_one = glob(base+"train/Type_1/*.jpg")+glob(base+"additional/Type_1/*.jpg")
+    train_files_two = glob(base+"train/Type_2/*.jpg")+glob(base+"additional/Type_2/*.jpg")
+    train_files_three = glob(base+"train/Type_3/*.jpg")+glob(base+"additional/Type_3/*.jpg")
     # Additional files
-    #train_files+= glob("/data/kaggle_3.27/additional/*/*.jpg")
-    shuffle(train_files)
-    train_files=train_files[0:samples]
+
+    shuffle(train_files_one)
+    shuffle(train_files_two)
+    shuffle(train_files_three)
+    train_files=train_files_one[0:samples]+train_files_two[0:samples]+train_files_three[0:samples]
     train_samples, validation_samples = train_test_split(train_files, test_size=0.2)
     old_model_name=None
     model_name="model"
@@ -143,26 +166,25 @@ def main(base,samples=5):
 
     print("train_samples",len(train_samples),train_samples[0])
     print("validation_samples",len(validation_samples),validation_samples[0])
-    history=train_model(model,train_samples,validation_samples,batch_size=64)
+    history=train_model(model,train_samples,validation_samples,batch_size=32)
     model.save(model_name+".h5")
     model.save_weights(model_name+"_weights.h5")
 
-    # predictions_df=pd.DataFrame()
-    # for x in range(0,len(test_files),400):
-    #     start=x
-    #     end=min(x+400,len(test_files))
-    #     test_image_df,test_imgs_mat = load_test_data(test_files[start:end],X_scaler)
-    #     preds=cv.predict(test_imgs_mat)
-    #     print("Test set predictions shape",preds.shape)
-    #     test_image_df["Type_1"]=preds[:,0]
-    #     test_image_df["Type_2"]=preds[:,1]
-    #     test_image_df["Type_3"]=preds[:,2]
-    #     predictions_df=predictions_df.append(test_image_df,ignore_index=True)
+    tg = test_generator(test_files, batch_size=64)
+    predictions_df=pd.DataFrame()
+    for X_test in tg:
+        output=model.predict(X_test)
+        test_image_df=pd.DataFrame({"Type_1":output[:,0],
+                                    "Type_2":output[:,1],
+                                    "Type_3":output[:,2],
+                                    })
+        predictions_df=predictions_df.append(test_image_df,ignore_index=True)
 
-    # func=lambda x: x.split("/")[-1]
-    # predictions_df["image_name"]=predictions_df["imagepath"].apply(func)
-    # predictions_df[["image_name","Type_1","Type_2","Type_3"]].to_csv(predictions_fi
+    predictions_df["imagepath"]=test_files
+    func=lambda x: x.split("/")[-1]
+    predictions_df["image_name"]=predictions_df["imagepath"].apply(func)
+    predictions_df[["image_name","Type_1","Type_2","Type_3"]].to_csv("nn.csv",index=False)
 
 if __name__=="__main__":
-    basepath="/home/carnd/cervix_type_prediction/data/"
-    main(basepath,30)
+    basepath="/data/"
+    main(basepath,1000)
